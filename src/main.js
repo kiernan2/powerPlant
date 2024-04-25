4cc2f3ae4579f566f37206a9074a343f60503c26
Kiernan2 <kiernan1994@gmail.com>
2024-04-25 15:08:35 -0700

    A functional program

--- src/main.js
+++ src/main.js
@@ -1,87 +1,78 @@
 const storeState = () => {
-  let currentState = {};
-  return (stateChangeFunction = state => state) => {
-    const newState = stateChangeFunction(currentState);
-    currentState = {...newState};
-    return newState;
+  let globalState = {};
+  return () => {
+    const plantNum = Object.keys(globalState).length + 1;
+    globalState[plantNum] = {plantNumber:plantNum};
+    return (stateChangeFunction = state => state) => {
+      const newState = stateChangeFunction(globalState[plantNum]);
+      globalState[plantNum] = {...newState};
+      return newState;
+    };
   };
 };
-
-const stateControl = storeState(); // The example program adds this but I'm not sure there's an actual purpose
-
-// const createState = (name) => {  // The entire function is weird and I'm not sure it was necessary in the first place
-//   let plant = {name: "", health:{soil: 0, light: 0, water: 0} }; // I need to create a new separate Plant I think I need to re-examine how I do this
-//   let newPlantDiv = document.getElementById("plants").innerHTML.createElement("div");
-//   newPlantDiv.id = name;
-//   newPlantDiv.append(); //Read 1227 and 1309 to better understand adding things to the HTML or Dom
-//   return plant; 
-// };
 
 const changeState = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
-      [prop]: (state[prop] || 0) + value
+      [prop]: (state[prop] || 0) + value,
     });
   };
 };
 
-const feed = changeState("soil")(1);
-const blueFood = changeState("soil")(5);
-const greenFood = changeState("soil")(10);
-const yuckyFood = changeState("soil")(-5);
+const feed = changeState("soil");
+const hydrate = changeState("water");
+// const giveLight = changeState("light");
 
-const hydrate = changeState("water")(1);
-const superWater = changeState("water")(5);
+const createPlant = (createPlantFun) => {
+  
+  const plantDiv = document.createElement("div");
+  const h2Element = document.createElement("h2");
+  const waterText = document.createElement("p");
+  const foodText = document.createElement("p");
+  const foodButton = document.createElement("button");
+  const waterButton = document.createElement("button");
+  
+  const plant = createPlantFun();
+  
+  foodButton.addEventListener("click", function() {
+    const newState = plant(feed(1));
+    foodText.innerText = `Food: ${newState.soil}`;
+  });
 
-const giveLight = changeState("light")(1);
-
-// Something might be wrong with the following two lines I'm not sure I have a starter plant so much set of undefined values
-const starterPlant = storeState;
-const plantStart = changeState(starterPlant)({Name: "starterPlant", health:{soil: 0, light: 0, water: 0}} );
-
-// const startFood = stateControl("feed")(1);
-// const startWater = stateControl("hydrate")(1);  // I don't know why but these three lines break everything for some reason
-// const startLight = stateControl("giveLight")(1);
-
-window.onload = function() {
-
-  document.getElementById('feedBlue').onclick = function() {
-    const newState = stateControl(blueFood);
-    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
-  };
-
-  document.getElementById('feedGreen').onclick = function() {
-    const newState = stateControl(greenFood);
-    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
-  };
-
-  document.getElementById('hydrate').onclick = function() {
-    const newState = stateControl(hydrate);
-    document.getElementById('water-value').innerText = `Water: ${newState.water}`;
-  };
-
-  document.getElementById('superWater').onclick = function() {
-    const newState = stateControl(superWater);
-    document.getElementById('water-value').innerText = `Water: ${newState.water}`;
-  };
-
-  document.getElementById('giveLight').onclick = function() {
-    const newState = stateControl(giveLight);
-    document.getElementById('light-value').innerText = `Light: ${newState.light}`;
-  };
-
-  // document.getElementById('createState').onclick = function() {
-  //   const newState = createState('testName');
-  //   document.getElementById('testName').innerText = `Name: ${newState.name}`; //The associated on click function for a function that's currently not working
-  // };
-
-  document.getElementById('show-state').onclick = function() {
-    const currentState = stateControl();
-    document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
-    document.getElementById('water-value').innerText = `Water: ${currentState.water}`;
-    document.getElementById('light-value').innerText = `light: ${currentState.light}`;
-  };
+  waterButton.addEventListener("click", function() {
+    const newState = plant(hydrate(1));
+    waterText.innerText = `Water: ${newState.water}`;
+  });
+  
+  h2Element.innerText = plant.name;
+  foodText.innerText = "Food: 0";
+  foodButton.innerText = "Feed";
+  waterText.innerText = "Water: 0";
+  waterButton.innerText = "Water";
+  
+  plantDiv.appendChild(h2Element);
+  plantDiv.appendChild(waterText);
+  plantDiv.appendChild(foodText);
+  plantDiv.appendChild(foodButton);
+  plantDiv.appendChild(waterButton);
+  
+  document.getElementById("plant").appendChild(plantDiv);
 };
 
+window.onload = function() {
+  
+  // The following line has to do with the apps global state
+  const mainState = storeState();
+  
+  document.getElementById('create-plant').onclick = function() {
+    createPlant(mainState); //Read 1227 and 1309 to better understand adding things to the HTML or Dom
+  };
 
+  // document.getElementById('show-state').onclick = function() {
+  //   const currentState = stateControl();
+  //   document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+  //   document.getElementById('water-value').innerText = `Water: ${currentState.water}`;
+  //   document.getElementById('light-value').innerText = `light: ${currentState.light}`;
+  // };
+};
